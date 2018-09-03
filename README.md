aa81486ef83a9866127b5e1e08ef2fa01d65f8f8
DoNotSpamPls <7570108+DoNotSpamPls@users.noreply.github.com>
2018-09-03 10:08:30 +0300

    Initial commit, adds /alert command

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# VelocityAlert
-An /alert command for Velocity
+# VelocityUtils
+VelocityUtils is a utility package for the new [Velocity Proxy](https://github.com/astei/velocity).
+It adds a lot of useful commands to aid admins and players alike with day to day use, ~~and is fully configurable~~ (soon)!
+
+## Commands
+
+VelocityUtils currently has the following commands
+* /alert (broadcasts a message to all players)
+
+These commands are currently planned
+* /find (tells you in which server a player is)
+* /list (lists players in all servers)
+* /send (sends a player to another server)
+
+As always, feel free to suggest new commands!
+
+## License
+VelocityUtils is licensed under the permissive MIT license. 
+Please see the [`LICENSE`](https://github.com/DoNotSpamPls/VelocityUtils/blob/master/LICENSE) file for more information.
---
+++ src/main/java/com/github/donotspampls/velocityutils/commands/AlertCommand.java
@@ -0,0 +1,47 @@
+package com.github.donotspampls.velocityutils.commands;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.command.Command;
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.util.LegacyChatColorUtils;
+import net.kyori.text.TextComponent;
+import net.kyori.text.format.TextColor;
+import net.kyori.text.serializer.ComponentSerializers;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import javax.annotation.Nonnull;
+import java.util.Collection;
+
+@SuppressWarnings("deprecation")
+public final class AlertCommand implements Command {
+
+    @Inject
+    private ProxyServer server;
+
+    public void execute(@Nonnull CommandSource source, @Nonnull String[] args) {
+        if (source.hasPermission("velocityutils.use")) {
+            if (args.length == 0) {
+                source.sendMessage(TextComponent.builder("You must supply a message.").color(TextColor.RED).build());
+            } else {
+                // Generate the message
+                StringBuilder builder = new StringBuilder();
+                builder.append(LegacyChatColorUtils.translate('&', "&8[&4Alert&8] &r"));
+                for ( String s : args ) {
+                    builder.append(LegacyChatColorUtils.translate( '&', s ));
+                    builder.append(" ");
+                }
+                @NonNull TextComponent message = ComponentSerializers.LEGACY.deserialize(builder.substring( 0, builder.length() - 1 ));
+
+                // Get all players and send the message
+                Collection<Player> players = server.getAllPlayers();
+                for (Player p : players) {
+                    p.sendMessage(message);
+                }
+            }
+        } else {
+            source.sendMessage(TextComponent.builder("You do not have permission to execute this command!").color(TextColor.RED).build());
+        }
+    }
+}
