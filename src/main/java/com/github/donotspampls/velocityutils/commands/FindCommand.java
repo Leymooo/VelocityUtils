c4c44ad3f730eee49d824c4c02da716cc7f3d501
Slava Maspanov <mikimouse100@mail.ru>
2018-09-29 10:18:21 +0300

    Code cleanup (#1)

--- src/main/java/com/github/donotspampls/velocityutils/commands/FindCommand.java
+++ src/main/java/com/github/donotspampls/velocityutils/commands/FindCommand.java
@@ -4,6 +4,7 @@ import com.velocitypowered.api.command.Command;
 import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.ServerConnection;
 import net.kyori.text.TextComponent;
 import net.kyori.text.format.TextColor;
 import net.kyori.text.serializer.ComponentSerializers;
@@ -11,10 +12,10 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.Optional;
 
-@SuppressWarnings({"deprecation", "OptionalGetWithoutIsPresent"})
 public class FindCommand implements Command {
 
     private final ProxyServer server;
+
     public FindCommand(ProxyServer server) {
         this.server = server;
     }
@@ -23,19 +24,19 @@ public class FindCommand implements Command {
     public void execute(@NonNull CommandSource source, String[] args) {
         if (source.hasPermission("velocityutils.find")) {
             if (args.length == 0) {
-                source.sendMessage(TextComponent.builder("Please follow this command by a user name").color(TextColor.RED).build());
+                source.sendMessage(TextComponent.of("Please follow this command by a user name", TextColor.RED));
             } else {
                 Optional<Player> player = server.getPlayer(args[0]);
                 if (!player.isPresent()) {
-                    source.sendMessage(TextComponent.builder("That user is not online").color(TextColor.RED).build());
+                    source.sendMessage(TextComponent.of("That user is not online", TextColor.RED));
                 } else {
                     Player pl = player.get();
-                    String srv = pl.getCurrentServer().get().getServerInfo().getName();
-                    pl.sendMessage(ComponentSerializers.LEGACY.deserialize("&e" + pl.getUsername() + " &ais online in server &e" + srv, '&'));
+                    Optional<ServerConnection> server = pl.getCurrentServer();
+                    server.ifPresent(srv -> pl.sendMessage(ComponentSerializers.LEGACY.deserialize("&e" + pl.getUsername() + " &ais online in server &e" + srv.getServerInfo().getName(), '&')));
                 }
             }
         } else {
-            source.sendMessage(TextComponent.builder("You do not have permission to execute this command!").color(TextColor.RED).build());
+            source.sendMessage(TextComponent.of("You do not have permission to execute this command!", TextColor.RED));
         }
     }
 
